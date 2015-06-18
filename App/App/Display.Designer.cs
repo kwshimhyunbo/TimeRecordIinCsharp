4241883bb0b94710e93a7125d3041439e5204b07
심현보 <adslbna2@naver.com>
2015-06-18 15:01:48 +0900

    test1

--- App/App/Display.Designer.cs
+++ App/App/Display.Designer.cs
@@ -49,16 +49,16 @@
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(79, 29);
+            this.textBox1.Location = new System.Drawing.Point(92, 27);
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.Size = new System.Drawing.Size(116, 21);
             this.textBox1.TabIndex = 1;
             // 
             // label1
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Location = new System.Drawing.Point(14, 8);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(46, 13);
             this.label1.TabIndex = 2;
@@ -67,9 +67,9 @@
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(12, 32);
+            this.label2.Location = new System.Drawing.Point(14, 30);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(64, 13);
+            this.label2.Size = new System.Drawing.Size(65, 12);
             this.label2.TabIndex = 3;
             this.label2.Text = "이름 검색 :";
             // 
@@ -77,35 +77,35 @@
             // 
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
             this.dataGridView1.Dock = System.Windows.Forms.DockStyle.Bottom;
-            this.dataGridView1.Location = new System.Drawing.Point(0, 129);
+            this.dataGridView1.Location = new System.Drawing.Point(0, 119);
             this.dataGridView1.Name = "dataGridView1";
-            this.dataGridView1.Size = new System.Drawing.Size(832, 214);
+            this.dataGridView1.Size = new System.Drawing.Size(971, 198);
             this.dataGridView1.TabIndex = 5;
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(12, 88);
+            this.label3.Location = new System.Drawing.Point(14, 81);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(61, 13);
+            this.label3.Size = new System.Drawing.Size(61, 12);
             this.label3.TabIndex = 6;
             this.label3.Text = "년도 검색:";
             // 
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(185, 88);
+            this.label4.Location = new System.Drawing.Point(216, 81);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(49, 13);
+            this.label4.Size = new System.Drawing.Size(49, 12);
             this.label4.TabIndex = 7;
             this.label4.Text = "월 검색:";
             // 
             // label5
             // 
             this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(346, 88);
+            this.label5.Location = new System.Drawing.Point(404, 81);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(49, 13);
+            this.label5.Size = new System.Drawing.Size(49, 12);
             this.label5.TabIndex = 8;
             this.label5.Text = "일 검색:";
             // 
@@ -113,7 +113,7 @@
             // 
             this.label6.AutoSize = true;
             this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label6.Location = new System.Drawing.Point(12, 64);
+            this.label6.Location = new System.Drawing.Point(14, 59);
             this.label6.Name = "label6";
             this.label6.Size = new System.Drawing.Size(63, 13);
             this.label6.TabIndex = 9;
@@ -121,23 +121,23 @@
             // 
             // textBox2
             // 
-            this.textBox2.Location = new System.Drawing.Point(79, 85);
+            this.textBox2.Location = new System.Drawing.Point(92, 78);
             this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.Size = new System.Drawing.Size(116, 21);
             this.textBox2.TabIndex = 10;
             // 
             // textBox3
             // 
-            this.textBox3.Location = new System.Drawing.Point(240, 85);
+            this.textBox3.Location = new System.Drawing.Point(280, 78);
             this.textBox3.Name = "textBox3";
-            this.textBox3.Size = new System.Drawing.Size(100, 20);
+            this.textBox3.Size = new System.Drawing.Size(116, 21);
             this.textBox3.TabIndex = 11;
             // 
             // button4
             // 
-            this.button4.Location = new System.Drawing.Point(507, 83);
+            this.button4.Location = new System.Drawing.Point(591, 77);
             this.button4.Name = "button4";
-            this.button4.Size = new System.Drawing.Size(55, 23);
+            this.button4.Size = new System.Drawing.Size(64, 21);
             this.button4.TabIndex = 14;
             this.button4.Text = "검색";
             this.button4.UseVisualStyleBackColor = true;
@@ -145,43 +145,43 @@
             // 
             // textBox4
             // 
-            this.textBox4.Location = new System.Drawing.Point(401, 85);
+            this.textBox4.Location = new System.Drawing.Point(468, 78);
             this.textBox4.Name = "textBox4";
-            this.textBox4.Size = new System.Drawing.Size(100, 20);
+            this.textBox4.Size = new System.Drawing.Size(116, 21);
             this.textBox4.TabIndex = 15;
             // 
             // label7
             // 
             this.label7.AutoSize = true;
-            this.label7.Location = new System.Drawing.Point(629, 88);
+            this.label7.Location = new System.Drawing.Point(734, 81);
             this.label7.Name = "label7";
-            this.label7.Size = new System.Drawing.Size(76, 13);
+            this.label7.Size = new System.Drawing.Size(77, 12);
             this.label7.TabIndex = 16;
             this.label7.Text = "총 근무시간: ";
             // 
             // label8
             // 
             this.label8.AutoSize = true;
-            this.label8.Location = new System.Drawing.Point(711, 88);
+            this.label8.Location = new System.Drawing.Point(829, 81);
             this.label8.Name = "label8";
-            this.label8.Size = new System.Drawing.Size(13, 13);
+            this.label8.Size = new System.Drawing.Size(11, 12);
             this.label8.TabIndex = 17;
             this.label8.Text = "0";
             // 
             // label9
             // 
             this.label9.AutoSize = true;
-            this.label9.Location = new System.Drawing.Point(711, 64);
+            this.label9.Location = new System.Drawing.Point(829, 59);
             this.label9.Name = "label9";
-            this.label9.Size = new System.Drawing.Size(79, 13);
+            this.label9.Size = new System.Drawing.Size(81, 12);
             this.label9.TabIndex = 18;
             this.label9.Text = "일. 시간:분:초";
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(568, 83);
+            this.button1.Location = new System.Drawing.Point(663, 77);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(55, 23);
+            this.button1.Size = new System.Drawing.Size(64, 21);
             this.button1.TabIndex = 19;
             this.button1.Text = "초기화";
             this.button1.UseVisualStyleBackColor = true;
@@ -189,9 +189,9 @@
             // 
             // Display
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(832, 343);
+            this.ClientSize = new System.Drawing.Size(971, 317);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.label9);
             this.Controls.Add(this.label8);
